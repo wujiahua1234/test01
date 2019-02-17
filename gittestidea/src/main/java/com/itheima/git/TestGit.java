1e9cc7138edf3ec873dcb6de2060c9341696be86
itheima <itheima@126.com>
2019-02-17 18:35:40 +0800

    提交到本地

---
+++ gittestidea/src/main/java/com/itheima/git/TestGit.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+}
